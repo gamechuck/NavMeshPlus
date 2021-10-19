7d8c582c188883e51525e0e6db8089bf979a30f9
2shady4u <jd16581556@gmail.com>
2021-10-19 13:41:11 +0200

    Fix crash on export due to undefined variable

--- NavMeshComponents/Scripts/NavMeshSurface2d.cs
+++ NavMeshComponents/Scripts/NavMeshSurface2d.cs
@@ -399,8 +399,10 @@ namespace UnityEngine.AI
                     var worldBounds = GetWorldBounds(localToWorld, new Bounds(m_Center, m_Size));
                     NavMeshBuilder.CollectSources(worldBounds, m_LayerMask, m_UseGeometry, m_DefaultArea, markups, sources);
                 }
+#if UNITY_EDITOR
                 if (!hideEditorLogs && !Mathf.Approximately(transform.eulerAngles.x, 270f))
                     Debug.LogWarning("NavMeshSurface2d is not rotated respectively to (x-90;y0;z0). Apply rotation unless intended.");
+#endif
                 var builder = new NavMeshBuilder2dWrapper();
                 builder.defaultArea = defaultArea;
                 builder.layerMask = layerMask;
